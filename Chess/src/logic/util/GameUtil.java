e47766206b37c4223edc72661ac21dfa33e6d7c6
pc <pc@DESKTOP-CJCCF16.lan>
2018-04-17 01:49:16 +0100

    update

--- Chess/src/logic/util/GameUtil.java
+++ Chess/src/logic/util/GameUtil.java
@@ -1,5 +1,7 @@
 package logic.util;
 
+import logic.pieces.Piece;
+
 public class GameUtil {
 	public enum PieceColor {
 		White, Black;
@@ -30,5 +32,48 @@ public class GameUtil {
 	
 	public static String toString(int x, int y) {
 		return (""+x)+(""+y);
+	}
+	
+	public boolean isPathFree(Piece[][] board, int x, int y, int dest_x, int dest_y) {
+		
+		if(board[x][y]==null) {
+			return false;
+		}
+		
+		if(board[dest_x][dest_y] != null) {
+			if(board[x][y].getColor() == board[dest_x][dest_y].getColor()) {
+				return false;
+			}
+		}
+
+		int dx = dest_x - x;
+		int dy = dest_y - y;
+		
+		if(dx == 0 && dy == 0) {
+			return false;
+		}
+		
+		int amount;
+
+		if(dx == 0 || dy == 0) {
+			amount = Math.abs(dx) + Math.abs(dy);
+		}
+		else if(Math.abs(dx) == Math.abs(dy)) {
+			amount = Math.abs(dx);
+		}
+		else {
+			return false;
+		}
+		
+		int vx = dx/amount;
+		int vy = dy/amount;
+		
+		for(int i = 1; i < amount; i++) {
+			if(board[x+i*vx][y+i*vy] != null) {
+				return false;
+			}
+		}
+
+		return true;
 	}
 }
