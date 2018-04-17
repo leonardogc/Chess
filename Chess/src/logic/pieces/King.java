3768e1c5d4a3413f97a9f72ebf0a43fce1a7dd87
pc <pc@DESKTOP-CJCCF16.lan>
2018-04-17 19:46:58 +0100

    updates

--- Chess/src/logic/pieces/King.java
+++ Chess/src/logic/pieces/King.java
@@ -1,6 +1,7 @@
 package logic.pieces;
 
 import logic.game.Game;
+import logic.util.GameUtil;
 import logic.util.GameUtil.PieceColor;
 import logic.util.GameUtil.PieceType;
 
@@ -12,7 +13,33 @@ public class King extends Piece{
 
 	@Override
 	public boolean move(int x, int y, int dest_x, int dest_y, Game game) {
-		return false;
+		if(!GameUtil.isPathFree(game.getBoard(), x, y, dest_x, dest_y)) {
+			return false;
+		}
+		
+		int dx = dest_x - x;
+		int dy = dest_y - y;
+		int amount;
+
+		if(dx == 0 || dy == 0) {
+			amount = Math.abs(dx) + Math.abs(dy);
+		}
+		else {
+			amount = Math.abs(dx);
+		}
+		
+		if(amount != 1) {
+			//TODO special move
+			return false;
+		}
+		
+		Piece[][] board = game.getBoard();
+		
+		board[dest_x][dest_y] = board[x][y];
+		
+		board[x][y] = null;
+		
+		return true;
 	}
 	
 	@Override
