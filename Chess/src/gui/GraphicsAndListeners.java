20ea957fbb62ea6fdb2254556ee036c998fde915
pc <pc@DESKTOP-CJCCF16.lan>
2018-04-21 02:03:11 +0100

    game working

--- Chess/src/gui/GraphicsAndListeners.java
+++ Chess/src/gui/GraphicsAndListeners.java
@@ -4,13 +4,18 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
+import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
 import logic.game.Game;
 import logic.game.Move;
 import logic.util.GameUtil;
+import logic.util.GameUtil.PieceColor;
 
 public class GraphicsAndListeners extends JPanel implements MouseListener{
 	
@@ -28,10 +33,44 @@ public class GraphicsAndListeners extends JPanel implements MouseListener{
 	private boolean sel_square;
 	public boolean move_piece;
 	
+	private BufferedImage rook_b;
+	private BufferedImage bishop_b;
+	private BufferedImage queen_b;
+	private BufferedImage pawn_b;
+	private BufferedImage king_b;
+	private BufferedImage knight_b;
+	
+	private BufferedImage rook_w;
+	private BufferedImage bishop_w;
+	private BufferedImage queen_w;
+	private BufferedImage pawn_w;
+	private BufferedImage king_w;
+	private BufferedImage knight_w;
+	
+	
 	public ConcurrentLinkedQueue<Move> queue;
 	
 	public GraphicsAndListeners() {
 		addMouseListener(this);
+		
+		try {
+			this.rook_b = ImageIO.read(new File("resources/rook_b.png"));
+			this.bishop_b = ImageIO.read(new File("resources/bishop_b.png"));
+			this.queen_b = ImageIO.read(new File("resources/queen_b.png"));
+			this.pawn_b = ImageIO.read(new File("resources/pawn_b.png"));
+			this.king_b = ImageIO.read(new File("resources/king_b.png"));
+			this.knight_b = ImageIO.read(new File("resources/knight_b.png"));
+			
+			this.rook_w = ImageIO.read(new File("resources/rook_w.png"));
+			this.bishop_w = ImageIO.read(new File("resources/bishop_w.png"));
+			this.queen_w = ImageIO.read(new File("resources/queen_w.png"));
+			this.pawn_w = ImageIO.read(new File("resources/pawn_w.png"));
+			this.king_w = ImageIO.read(new File("resources/king_w.png"));
+			this.knight_w = ImageIO.read(new File("resources/knight_w.png"));
+		} 
+		catch (IOException e) {
+			System.out.println("Could not read images");
+		}
 		
 		this.queue = new ConcurrentLinkedQueue<>();
 		
@@ -48,7 +87,10 @@ public class GraphicsAndListeners extends JPanel implements MouseListener{
 	@Override
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
+		Game copy = this.game.makeCopy();
 		drawBoard(g);
+		drawPieces(g, copy);
+		g.drawImage(rook_b,dx,dy,square_size,square_size, null);
 	}
 	
 	private void drawBoard(Graphics g) {
@@ -80,8 +122,66 @@ public class GraphicsAndListeners extends JPanel implements MouseListener{
 		}
 	}
 	
-	private void drawPieces(Graphics g) {
-		
+	private void drawPieces(Graphics g, Game game) {
+		for(int y=0; y < GameUtil.boardSize; y++) {
+			for(int x=0; x < GameUtil.boardSize; x++) {
+				if(game.getBoard()[x][y] != null) {
+					int pos_x = dx + x*square_size;
+					int pos_y = dy + (GameUtil.boardSize-1-y)*square_size;
+					
+					switch(game.getBoard()[x][y].getType()) {
+					case King:
+						if(game.getBoard()[x][y].getColor() == PieceColor.White) {
+							g.drawImage(king_w,pos_x,pos_y,square_size,square_size, null);
+						}
+						else {
+							g.drawImage(king_b,pos_x,pos_y,square_size,square_size, null);
+						}
+						break;
+					case Rook:
+						if(game.getBoard()[x][y].getColor() == PieceColor.White) {
+							g.drawImage(rook_w,pos_x,pos_y,square_size,square_size, null);
+						}
+						else {
+							g.drawImage(rook_b,pos_x,pos_y,square_size,square_size, null);
+						}
+						break;
+					case Queen:
+						if(game.getBoard()[x][y].getColor() == PieceColor.White) {
+							g.drawImage(queen_w,pos_x,pos_y,square_size,square_size, null);
+						}
+						else {
+							g.drawImage(queen_b,pos_x,pos_y,square_size,square_size, null);
+						}
+						break;
+					case Pawn:
+						if(game.getBoard()[x][y].getColor() == PieceColor.White) {
+							g.drawImage(pawn_w,pos_x,pos_y,square_size,square_size, null);
+						}
+						else {
+							g.drawImage(pawn_b,pos_x,pos_y,square_size,square_size, null);
+						}
+						break;
+					case Knight:
+						if(game.getBoard()[x][y].getColor() == PieceColor.White) {
+							g.drawImage(knight_w,pos_x,pos_y,square_size,square_size, null);
+						}
+						else {
+							g.drawImage(knight_b,pos_x,pos_y,square_size,square_size, null);
+						}
+						break;
+					case Bishop:
+						if(game.getBoard()[x][y].getColor() == PieceColor.White) {
+							g.drawImage(bishop_w,pos_x,pos_y,square_size,square_size, null);
+						}
+						else {
+							g.drawImage(bishop_b,pos_x,pos_y,square_size,square_size, null);
+						}
+						break;
+					}
+				}
+			}
+		}
 	}
 
 	@Override
