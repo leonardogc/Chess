e4241246a39feaa05ce3340c5f5911baa20af29b
leonardogc <leonardogomescapozzi@gmail.com>
2018-06-04 16:25:58 +0100

    update

--- Chess/src/logic/ai/Minimax.java
+++ Chess/src/logic/ai/Minimax.java
@@ -26,12 +26,12 @@ public class Minimax {
 	
 	
 
-	public static final double king_score = 20000;
-	public static final double queen_score = 900;
-	public static final double rook_score = 500;
-	public static final double knight_score = 320;
-	public static final double bishop_score = 330;
-	public static final double pawn_score = 100;
+	public static final int king_score = 200000;
+	public static final int queen_score = 9000;
+	public static final int rook_score = 5000;
+	public static final int knight_score = 3200;
+	public static final int bishop_score = 3300;
+	public static final int pawn_score = 1000;
 	
 	//for white pieces
 	//use mirrored for black pieces
@@ -100,12 +100,12 @@ public class Minimax {
 														  
 														  
 														  
-    public static final double king_score_v2 = 200000;
-	public static final double queen_score_v2 = 9000;
-	public static final double rook_score_v2 = 5000;
-	public static final double knight_score_v2 = 3000;
-	public static final double bishop_score_v2 = 3000;
-	public static final double pawn_score_v2 = 1000;
+   /* public static final int king_score_v2 = 200000;
+	public static final int queen_score_v2 = 9000;
+	public static final int rook_score_v2 = 5000;
+	public static final int knight_score_v2 = 3000;
+	public static final int bishop_score_v2 = 3000;
+	public static final int pawn_score_v2 = 1000;
 	
 	//for white pieces
 	//use mirrored for black pieces
@@ -176,8 +176,96 @@ public class Minimax {
 															 {0, -100, 40, 75, 75, 0, 0, 0},
 															 {0, 0, 0, 15, 0, 0, 0, 0},
 															 {0, 0, 0, 0, 0, 0, 0, 0},
-															 {0, 0, 0, 0, 0, 0, 0, 0}};
+															 {0, 0, 0, 0, 0, 0, 0, 0}};*/
+															 
+															 
+															 
+    public static final int king_score_v2 = 20000;
+	public static final int queen_score_v2 = 975;
+	public static final int rook_score_v2 = 500;
+	public static final int knight_score_v2 = 325;
+	public static final int bishop_score_v2 = 335;
+	public static final int pawn_score_v2 = 100;
+	
+	public static final int bishop_pair = 30;
+	public static final int knight_pair = 8;
+	public static final int rook_pair = 16;
+	
+	public static final int[] knight_adj = new int[]{ -20, -16, -12, -8, -4,  0,  4,  8, 12};
+	public static final int[] rook_adj = new int[]{  15,  12,   9,  6,  3,  0, -3, -6, -9};
+
+	
+	//for white pieces
+	//use mirrored for black pieces
+	public static final int[][] king_board_middle_v2 = new int[][] {{40, 30, 10, 0, -10, -20, -30, -40},
+																	{50, 40, 20, 10, 0, -10, -20, -30},
+																	{30, 20, 0, -10, -20, -30, -40, -50},
+																	{10, 0, -20, -30, -40, -50, -60, -70},
+																	{10, 0, -20, -30, -40, -50, -60, -70},
+																	{30, 20, 0, -10, -20, -30, -40, -50},
+																	{50, 40, 20, 10, 0, -10, -20, -30},
+																	{40, 30, 10, 0, -10, -20, -30, -40}};
+																	
+															
+	public static final int[][] king_board_end_v2 = new int[][] {{-72, -48, -36, -24, -24, -36, -48, -72},
+																 {-48, -24, -12, 0, 0, -12, -24, -48},
+																 {-36, -12, 0, 12, 12, 0, -12, -36},
+																 {-24, 0, 12, 24, 24, 12, 0, -24},
+																 {-24, 0, 12, 24, 24, 12, 0, -24},
+																 {-36, -12, 0, 12, 12, 0, -12, -36},
+																 {-48, -24, -12, 0, 0, -12, -24, -48},
+																 {-72, -48, -36, -24, -24, -36, -48, -72}};
+																	
+															
+	public static final int[][] queen_board_v2 = new int[][] {{-5, 0, 0, 0, 0, 0, 0, 0},
+															  {-5, 0, 0, 0, 0, 0, 0, 0},
+															  {-5, 1, 1, 2, 2, 1, 1, 0},
+															  {-5, 1, 2, 3, 3, 2, 1, 0},
+															  {-5, 1, 2, 3, 3, 2, 1, 0},
+															  {-5, 1, 1, 2, 2, 1, 1, 0},
+															  {-5, 0, 0, 0, 0, 0, 0, 0},
+															  {-5, 0, 0, 0, 0, 0, 0, 0}};
+															  
+														   
+	public static final int[][] rook_board_v2 = new int[][] {{0, -5, -5, -5, -5, -5, 20, 5},
+															 {0, 0, 0, 0, 0, 0, 20, 5},
+															 {0, 0, 0, 0, 0, 0, 20, 5},
+															 {2, 0, 0, 0, 0, 0, 20, 5},
+															 {2, 0, 0, 0, 0, 0, 20, 5},
+															 {0, 0, 0, 0, 0, 0, 20, 5},
+															 {0, 0, 0, 0, 0, 0, 20, 5},
+															 {0, -5, -5, -5, -5, -5, 20, 5}};
+		  													 
 														  
+	public static final int[][] knight_board_v2 = new int[][] {{-8, -8, -8, -8, -8, -8, -8, -8},
+															   {-12, 0, 0, 0, 0, 0, 0, -8},
+															   {-8, 1, 4, 4, 4, 4, 0, -8},
+															   {-8, 2, 4, 8, 8, 4, 0, -8},
+															   {-8, 2, 4, 8, 8, 4, 0, -8},
+															   {-8, 1, 4, 4, 4, 4, 0, -8},
+															   {-12, 0, 0, 0, 0, 0, 0, -8},
+															   {-8, -8, -8, -8, -8, -8, -8, -8}};
+															   
+															   
+															
+	public static final int[][] bishop_board_v2 = new int[][] {{-4, -4, -4, -4, -4, -4, -4, -4},
+															   {-4, 2, 1, 0, 0, 0, 0, -4},
+															   {-12, 1, 2, 4, 4, 2, 0, -4},
+															   {-4, 1, 4, 6, 6, 4, 0, -4},
+															   {-4, 1, 4, 6, 6, 4, 0, -4},
+															   {-12, 1, 2, 4, 4, 2, 0, -4},
+															   {-4, 2, 1, 0, 0, 0, 0, -4},
+															   {-4, -4, -4, -4, -4, -4, -4, -4}};
+															
+	public static final int[][] pawn_board_v2 = new int[][] {{0, -6, -4, -6, -6, -6, -6, 0},
+															 {0, -4, -4, -4, -4, -4, -4, 0},
+															 {0, 1, 1, 5, 2, 1, 1, 0},
+															 {0, -24, 5, 10, 8, 2, 1, 0},
+															 {0, -24, 5, 10, 8, 2, 1, 0},
+															 {0, 1, 1, 5, 2, 1, 1, 0},
+															 {0, -4, -4, -4, -4, -4, -4, 0},
+															 {0, -6, -4, -6, -6, -6, -6, 0}};														 
+  
 	
 
 	
@@ -541,11 +629,15 @@ public class Minimax {
 		boolean white_queen = false;
 		boolean black_queen = false;
 		
-		int white_minor_pieces = 0;
-		int black_minor_pieces = 0;
+		int n_pawns_w = 0;
+		int n_knights_w = 0;
+		int n_bishops_w = 0;
+		int n_rooks_w = 0;
 		
-		int white_major_pieces = 0;
-		int black_major_pieces = 0;
+		int n_pawns_b = 0;
+		int n_knights_b = 0;
+		int n_bishops_b = 0;
+		int n_rooks_b = 0;
 		
 		int black_king_x = -1;
 		int black_king_y = -1;
@@ -573,12 +665,12 @@ public class Minimax {
 						if(game.getBoard()[x][y].getColor() == PieceColor.White) {
 							white_score+=rook_score_v2;
 							white_score+=rook_board_v2[x][y];
-							white_major_pieces++;
+							n_rooks_w++;
 						}
 						else {
 							black_score+=rook_score_v2;
 							black_score+=rook_board_v2[x][GameUtil.boardSize-1-y];
-							black_major_pieces++;
+							n_rooks_b++;
 						}
 						break;
 					case Queen:
@@ -586,47 +678,47 @@ public class Minimax {
 							white_score+=queen_score_v2;
 							white_score+=queen_board_v2[x][y];
 							white_queen = true;
-							white_major_pieces++;
 						}
 						else {
 							black_score+=queen_score_v2;
 							black_score+=queen_board_v2[x][GameUtil.boardSize-1-y];
 							black_queen = true;
-							black_major_pieces++;
 						}
 						break;
 					case Pawn:
 						if(game.getBoard()[x][y].getColor() == PieceColor.White) {
 							white_score+=pawn_score_v2;
 							white_score+=pawn_board_v2[x][y];
+							n_pawns_w++;
 						}
 						else {
 							black_score+=pawn_score_v2;
 							black_score+=pawn_board_v2[x][GameUtil.boardSize-1-y];
+							n_pawns_b++;
 						}
 						break;
 					case Knight:
 						if(game.getBoard()[x][y].getColor() == PieceColor.White) {
 							white_score+=knight_score_v2;
 							white_score+=knight_board_v2[x][y];
-							white_minor_pieces++;
+							n_knights_w++;
 						}
 						else {
 							black_score+=knight_score_v2;
 							black_score+=knight_board_v2[x][GameUtil.boardSize-1-y];
-							black_minor_pieces++;
+							n_knights_b++;
 						}
 						break;
 					case Bishop:
 						if(game.getBoard()[x][y].getColor() == PieceColor.White) {
 							white_score+=bishop_score_v2;
 							white_score+=bishop_board_v2[x][y];
-							white_minor_pieces++;
+							n_bishops_w++;
 						}
 						else {
 							black_score+=bishop_score_v2;
 							black_score+=bishop_board_v2[x][GameUtil.boardSize-1-y];
-							black_minor_pieces++;
+							n_bishops_b++;
 						}
 						break;
 					}
@@ -639,13 +731,13 @@ public class Minimax {
 		if(!white_queen && !black_queen) {
 			endgame = true;
 		}
-		else if(!white_queen && (black_queen && black_minor_pieces <= 1 && black_major_pieces == 1)) {
+		else if(!white_queen && (black_queen && (n_bishops_b+n_knights_b) <= 1 && n_rooks_b == 0)) {
 			endgame = true;
 		}
-		else if(!black_queen && (white_queen && white_minor_pieces <= 1 && white_major_pieces == 1)) {
+		else if(!black_queen && (white_queen && (n_bishops_w+n_knights_w) <= 1 && n_rooks_w == 0)) {
 			endgame = true;
 		}
-		else if((black_queen && black_minor_pieces <= 1 && black_major_pieces == 1) && (white_queen && white_minor_pieces <= 1 && white_major_pieces == 1)) {
+		else if((black_queen && (n_bishops_b+n_knights_b) <= 1 && n_rooks_b == 0) && (white_queen && (n_bishops_w+n_knights_w) <= 1 && n_rooks_w == 0)) {
 			endgame = true;
 		}
 		
@@ -657,7 +749,38 @@ public class Minimax {
 			white_score+=king_board_middle_v2[white_king_x][white_king_y];
 			black_score+=king_board_middle_v2[black_king_x][GameUtil.boardSize-1-black_king_y];
 		}
+
+		if(n_bishops_w > 1) {
+			white_score += bishop_pair;
+		}
+
+		if(n_rooks_w > 1) {
+			white_score -= rook_pair;
+		}
+
+		if(n_knights_w > 1) {
+			white_score -= knight_pair;
+		}
 		
+		
+		if(n_bishops_b > 1) {
+			black_score += bishop_pair;
+		}
+
+		if(n_rooks_b > 1) {
+			black_score -= rook_pair;
+		}
+
+		if(n_knights_b > 1) {
+			black_score -= knight_pair;
+		}
+		
+		white_score += n_knights_w*knight_adj[n_pawns_w];
+		white_score += n_rooks_w*rook_adj[n_pawns_w];
+		
+		black_score += n_knights_b*knight_adj[n_pawns_b];
+		black_score += n_rooks_b*rook_adj[n_pawns_b];
+
 		if(max == PieceColor.White) {
 			return (white_score - black_score); 
 		}
@@ -666,6 +789,7 @@ public class Minimax {
 		}
 		
 	}
+	
 	
 	public static int win_loss_score(Turn turn, int node) {
 		if(turn == Turn.Max) {
